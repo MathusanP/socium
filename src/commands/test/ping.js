a5b4ba55055d1e240f1dcff743ae8c1a5733bb93
agnag <mathuyt141@gmail.com>
2022-10-30 14:24:30 +0000

    feat(ping.js): Added ping command

---
+++ src/commands/test/ping.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	name: 'ping',
+	description: 'Shows the latency for the bot',
+	usage: '',
+
+	permissions: [],
+	ownerOnly: false,
+	guildOnly: true,
+
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Shows the latency for the bot!'),
+
+	error: false,
+	execute: ({ interaction, client }) => {
+
+		const embed = new EmbedBuilder()
+			.setTitle('Pong!')
+			.setDescription(`The number no one has asked for is: ${client.ws.ping}ms.`)
+			.setColor('#FF0000')
+			.setTimestamp();
+
+		interaction.followUp({ embeds: [embed] });
+
+	},
+};
